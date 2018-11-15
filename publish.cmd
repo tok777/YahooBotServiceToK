f5141dabc55a7a157a393036d79746ef7b65e9bf
tok777 <30418577+tok777@users.noreply.github.com>
2018-11-15 21:32:33 +0100

    Projektdateien hinzufügen.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=yahoobotservicetok-Web-Deploy.pubxml -p:Password=414vnDRccqCqgsnZfHSRvPyKFBKXuABDtzuBm27EkQiPsP4rnthbxfLYGufZ
+
